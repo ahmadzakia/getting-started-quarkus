41ef5945c28b86ef316b1be9e71c2e03325079bb
ahmadzakia <zaki@traveloka.com>
2020-12-05 07:20:48 +0700

    add gitignore

---
+++ src/main/java/org/acme/getting/started/getting/started/GreetingService.java
@@ -0,0 +1,9 @@
+/**
+ * Traveloka.com
+ */
+package org.acme.getting.started.getting.started;/**
+ * @author ahmadzakianshori
+ * @version GreetingService.java, v 0.1 05-12-2020 06.33
+ */
+public class GreetingService {
+}
